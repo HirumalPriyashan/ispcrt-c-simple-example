c48efb3696c206d4e7dd7a236e405f1a84538b66
Hirumal Priyashan <hirupriyashanrc@gmail.com>
2023-03-05 15:24:21 +0530

    vec add without callocs for pointers

--- simple.c
+++ simple.c
@@ -16,44 +16,50 @@ static int run(const ISPCRTDeviceType device_type, const unsigned int N) {
     for (int i = 0; i < N; i++)
         a[i] = i, b[i] = N - i;
     ISPCRTDevice device = ispcrtGetDevice(device_type, 0);
+    ISPCRTTaskQueue queue = ispcrtNewTaskQueue(device);
 
     ISPCRTNewMemoryViewFlags flags;
     flags.allocType = ISPCRT_ALLOC_TYPE_DEVICE;
     ISPCRTMemoryView a_dev = ispcrtNewMemoryView(device, a, sizeof(float) * N, &flags);
     ISPCRTMemoryView b_dev = ispcrtNewMemoryView(device, b, sizeof(float) * N, &flags);
     ISPCRTMemoryView c_dev = ispcrtNewMemoryView(device, c, sizeof(float) * N, &flags);
+    ispcrtCopyToDevice(queue, a_dev);
+    ispcrtCopyToDevice(queue, b_dev);
+    ispcrtCopyToDevice(queue, c_dev);
 
-    size_t size = 3 * sizeof(float *) + 4 * sizeof(int);
+    // err = nomp_run(id, 4, "a", NOMP_PTR, sizeof(TEST_TYPE), a, "b", NOMP_PTR,
+    //              sizeof(TEST_TYPE), b, "c", NOMP_PTR, sizeof(TEST_TYPE), c, "N",
+    //              NOMP_INTEGER, sizeof(int), &n);
+    size_t size = 3 * sizeof(float *) + 3 * sizeof(int);
     void **p = (void **)calloc(1, size);
-    p[0] = (float*)ispcrtDevicePtr(a_dev);
-    p[1] = (float*)ispcrtDevicePtr(b_dev);
-    p[2] = (float*)ispcrtDevicePtr(c_dev);
+    // p[0] = calloc(1, (sizeof(float *)));
+    p[0] = ispcrtDevicePtr(a_dev);
+    // p[1] = calloc(1, (sizeof(float *)));
+    p[1] = ispcrtDevicePtr(b_dev);
+    // p[2] = calloc(1, (sizeof(float *)));
+    p[2] = ispcrtDevicePtr(c_dev);
     p[3] = calloc(1, (sizeof(int)));
     *((int *)(p[3])) = N;
     p[4] = calloc(1, (sizeof(int)));
-    *((int *)(p[4])) = N;
+    *((int *)(p[4])) = 1;
     p[5] = calloc(1, (sizeof(int)));
     *((int *)(p[5])) = 1;
-    p[6] = calloc(1, (sizeof(int)));
-    *((int *)(p[6])) = 1;
 
     ISPCRTMemoryView p_dev = ispcrtNewMemoryView(device, p, size, &flags);
 
     ISPCRTModuleOptions options = {};
     ISPCRTModule module = ispcrtLoadModule(device, "simple", options);
-    ISPCRTKernel kernel = ispcrtNewKernel(device, module, "vec_add_inner");
+    if (rt_error != ISPCRT_NO_ERROR)
+        printf("%s ", err_message);
+    ISPCRTKernel kernel = ispcrtNewKernel(device, module, "main_ispc");
 
-    ISPCRTTaskQueue queue = ispcrtNewTaskQueue(device);
     ispcrtCopyToDevice(queue, p_dev);
-    ispcrtCopyToDevice(queue, a_dev);
-    ispcrtCopyToDevice(queue, b_dev);
-    // ispcrtCopyToDevice(queue, c_dev);
     void *res = ispcrtLaunch3D(queue, kernel, p_dev, 1, 1, 1);
     ispcrtCopyToHost(queue, c_dev);
     ispcrtSync(queue);
 
     for (unsigned i = 0; i < N; i++)
-        printf("%f", c[i]);
+        printf("%f ", c[i]);
     free(p);
     return 0;
 }
