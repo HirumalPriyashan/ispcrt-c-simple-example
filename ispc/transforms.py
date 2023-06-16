886a1889b9207b90ae3be9a90f888de66f5273df
Hirumal Priyashan <hirupriyashanrc@gmail.com>
2023-06-16 16:56:17 +0530

    Add perf files

---
+++ ispc/transforms.py
@@ -0,0 +1,13 @@
+import sys
+
+import loopy as lp
+
+LOOPY_LANG_VERSION = (2018, 2)
+
+
+def foo(knl, context):
+    backend = context["backend"]
+    split_size = 8 if (backend == "ispc") else 32
+    knl = lp.split_iname(knl, "i", split_size)
+    knl = lp.tag_inames(knl, [("i_outer", "g.0"), ("i_inner", "l.0")])
+    return knl
